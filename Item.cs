74e1f95e9973dfc81b85bbfd822bd035055bad5d
Kristy Prieto <kprieto14@gmail.com>
2023-05-02 20:29:50 -0400

    Finished explorer mode.

---
+++ Item.cs
@@ -0,0 +1,8 @@
+namespace ApiClient
+{
+    public class Item
+    {
+        public string message { get; set; }
+        public string status { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace ApiClient
 {
   class Program
   {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-      Console.WriteLine("Welcome to C#");
+      var client = new HttpClient();
+
+      var responseBody = await client.GetStreamAsync("https://dog.ceo/api/breeds/image/random");
+
+      var items = await JsonSerializer.DeserializeAsync<Item>(responseBody);
+
+      Console.WriteLine("\nWelcome to my beginner API app! ");
+      Console.WriteLine($"{items.status}! Here is your random dog's URL 🐶 : {items.message}. I wish you could see how cute it is.\n");
     }
   }
 }
